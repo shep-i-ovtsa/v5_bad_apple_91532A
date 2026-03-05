8b38ef77a1b992e5e86010eee6d96965ac356163
natali <shepsemail1@gmail.com>
2026-03-05 12:53:48 -0700

    updated example
    Added set_path method call to allocate memory for bad apple task.

--- src/main.cpp
+++ src/main.cpp
@@ -5,8 +5,9 @@ void initalise(){
  
 }
 void opcontrol(){   
-    pros::Task bad(bad_apple::play, &apple, TASK_PRIORITY_DEFAULT, 16384, "BadApple"); 
+    pros::Task bad(bad_apple::play, &apple, TASK_PRIORITY_DEFAULT, 16384, "BadApple"); //in the link version this will still play but its required to upload a link to your video using the set set_path() function
     //VERY IMPORTANT!!! YOU MUST CALL IT LIKE THIS TO ALLOCATE 16kbs to stack or more. if not you will RUN OUT OF MEMORY! (im too tired to push it so just copy paste this ok)
+    apple.set_path("/usd/bad_apple.txt");
     pros::Controller master(pros::E_CONTROLLER_MASTER);    
     apple.set_speed(speed_multipliers::ONE);
     while(true){
